87047358df61004ba695f0f7ffb5390bb94a757a
Fuyu <182488764+fuyuz@users.noreply.github.com>
2024-09-23 23:02:30 +0900

    Homebrewのセットアップを追加

---
+++ .bin/homebrew/setup.homebrew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+echo "start homebrew setup"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BREWFILE="$SCRIPT_DIR/Brewfile"
+
+if [ $(uname) = Darwin ]; then
+  if ! type brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  else
+    echo "nothing happened since homebrew is already installed."
+  fi
+
+  brew bundle -file=$Brewfile
+fi
+
+echo "finish homebrew setup"
