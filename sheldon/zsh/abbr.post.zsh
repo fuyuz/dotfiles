170ceb30357210f27bff9e2fb7ddf68addbfa711
Fuyu <182488764+fuyuz@users.noreply.github.com>
2024-09-25 23:37:24 +0900

    sheldonの設定ファイルを追加

---
+++ sheldon/zsh/abbr.post.zsh
@@ -0,0 +1,13 @@
+# git
+abbr g='git'
+abbr gp='git push'
+abbr gpf='git push --force'
+
+# vim
+abbr v='vim'
+alias vim='nvim'
+
+# others
+alias ls='ls --color=auto'
+abbr l='ls -l'
+abbr la='ls -la'
