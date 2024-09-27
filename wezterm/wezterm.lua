9c39e8139fc53329609ba76d72bd19727b6f8192
Fuyu <182488764+fuyuz@users.noreply.github.com>
2024-09-28 00:03:36 +0900

    weztermのPanelの設定を追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,4 +14,50 @@ config.colors = { background = 'rgb(0,0,0)' }
 -- mac option key settigns
 config.send_composed_key_when_left_alt_is_pressed = true
 
+-- window settings
+config.leader = {
+  key = 'q',
+  mods = 'CTRL',
+  timeout_milliseconds = 2000
+}
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "-",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods = "LEADER",
+    key = "=",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action { ActivatePaneDirection = 'Left' }
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action { ActivatePaneDirection = 'Down' }
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action { ActivatePaneDirection = 'Up' }
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action { ActivatePaneDirection = 'Right' }
+  }
+}
+
+ config.window_decorations = "RESIZE"
+ config.show_new_tab_button_in_tab_bar = false
+ config.show_close_tab_button_in_tabs = false
+
+-- stop termianl bells
+config.audible_bell = "Disabled"
+
 return config
