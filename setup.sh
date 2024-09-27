72319b40aebe5b5f4e8be43f9125245baab0130d
Fuyu <182488764+fuyuz@users.noreply.github.com>
2024-09-27 22:55:24 +0900

    nvimのセットアップファイルを追加

--- setup.sh
+++ setup.sh
@@ -5,7 +5,14 @@ set -eu
 echo "start dotfiles setup"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-FILES=(homebrew/setup.homebrew.sh rectangle/setup.rectangle.sh sheldon/setup.sheldon.sh starship/setup.starship.sh wezterm/setup.wezterm.sh)
+FILES=(
+homebrew/setup.homebrew.sh
+rectangle/setup.rectangle.sh
+sheldon/setup.sheldon.sh
+starship/setup.starship.sh
+wezterm/setup.wezterm.sh
+nvim/setup.nvim.sh
+)
 
 for file in "${FILES[@]}"; do
   sh "$SCRIPT_DIR/$file"
