1687781d094f4fa14f7a3260c20abb1b6f5b2ecb
fuyu <182488764+fuyuz@users.noreply.github.com>
2025-01-23 11:23:20 +0900

    諸々修正

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,7 @@ vim.opt.wrap = true
 vim.opt.swapfile = false
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
