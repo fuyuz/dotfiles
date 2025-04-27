275b64d526870281610754123d89fc137861f7a0
Fuyuz <182488764+fuyuz@users.noreply.github.com>
2025-04-27 14:21:56 +0900

    設定ファイルを修正

--- sheldon/setup.sheldon.sh
+++ sheldon/setup.sheldon.sh
@@ -9,6 +9,7 @@ FILES=(plugins.toml zsh/abbr.post.zsh zsh/fzf.post.zsh)
 CONFIG_DIR="$HOME/.config/sheldon"
 
 mkdir -p "$CONFIG_DIR"
+mkdir -p "$CONFIG_DIR/zsh"
 for file in "${FILES[@]}"; do
   SOURCE="$SCRIPT_DIR/$file"
   TARGET="$CONFIG_DIR/$file"
